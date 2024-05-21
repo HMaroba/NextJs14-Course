87a4ec55ed07e213cff92e9e09e4fbaad9034b26
Coffee58511867 <marobahlalele910@gmail.com>
2024-05-21 18:03:35 +0200

    Initial Commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "About Maroba",
+  description: "Maroba is Developer",
+};
+
+export default function About() {
+  return (
+    <div>
+      <p>About us</p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Next JS 14 FULL COURSE</p>
-         </main>
+      <p className="text-4xl font-medium">Next JS 14 FULL COURSE</p>
+    </main>
   );
 }
