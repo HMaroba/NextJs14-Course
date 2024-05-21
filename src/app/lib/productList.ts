9a31c5837e85a0e1d6f3addbf988c638690da4af
Coffee58511867 <marobahlalele910@gmail.com>
2024-05-21 21:03:11 +0200

    Initial Commit

--- src/app/lib/productList.ts
+++ src/app/lib/productList.ts
@@ -2,46 +2,55 @@ export const productsList = [
   {
     id: 1,
     title: "Laptop",
+    image: 'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     description: "Laptop for Sale here",
   },
   {
     id: 2,
     title: "Phone",
+    image: 'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     description: "Phone for Sale here",
   },
   {
     id: 3,
     title: "Desktop",
+    image: 'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     description: "Desktop for Sale here",
   },
   {
     id: 4,
     title: "Tablet",
+    image: 'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     description: "Tablet for Sale here",
   },
   {
     id: 5,
     title: "Smartwatch",
+    image: 'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     description: "Smartwatch for Sale here",
   },
   {
     id: 6,
     title: "Monitor",
+    image: 'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     description: "Monitor for Sale here",
   },
   {
     id: 7,
     title: "Keyboard",
+    image: 'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     description: "Keyboard for Sale here",
   },
   {
     id: 8,
     title: "Mouse",
+    image: 'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     description: "Mouse for Sale here",
   },
   {
     id: 9,
     title: "Printer",
+    image: 'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     description: "Printer for Sale here",
   },
 ];
