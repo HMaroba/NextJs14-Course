3ff8c95eaec785b6fb2342448a0cd1e19c4f4bae
Coffee58511867 <marobahlalele910@gmail.com>
2024-05-21 19:35:31 +0200

    Initial Commit

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,31 @@
+import { productsList } from "@/app/lib/productList";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  try {
+    const product =  productsList.find((item: any) => item.id == id);
+
+
+    const productId = product?.id;
+
+    if (!productId) {
+      return NextResponse.json({
+        message: "Product does not exists",
+        success: false,
+      });
+    }
+
+    return NextResponse.json(product);
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong on the server.",
+      status: 500,
+    });
+  }
+}
--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -7,19 +7,35 @@ type Props = {
   };
 };
 
+async function getData(id: string) {
+  const res = await fetch(`http:localhost:3000/api/products/${id}`);
 
-export const generateMetadata = ({params} : Props) : Metadata => {
-    return{
-        title: `Product ${params.productId}`,
-        openGraph: {
-            images: ['https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Flaptops%2F&psig=AOvVaw3X79l6D6fOVUeFRaMwyqZY&ust=1716395653303000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDpvZuWn4YDFQAAAAAdAAAAABAE'],
-          },
-    }
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
-export  default function ProductDetails({ params }: Props) {
-    // const productData = productsList.find((item: any) => item.id = params.productId); 
-    // console.log(productData);
-    
+
+export  const generateMetadata = ({ params }: Props): Metadata => {
+  const data =  getData(params.productId);
+  console.log('Data' +data);
+  
+  return {
+    title: `Product ${params.productId}`,
+    openGraph: {
+      images: [
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Flaptops%2F&psig=AOvVaw3X79l6D6fOVUeFRaMwyqZY&ust=1716395653303000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDpvZuWn4YDFQAAAAAdAAAAABAE",
+      ],
+    },
+  };
+};
+export default async function ProductDetails({ params }: Props) {
+  // const productData = productsList.find((item: any) => item.id = params.productId);
+  // console.log(productData);
+  const data = await getData(params.productId);
+  
+
   return (
     <div className="p-5">
       <p>Details for Product {params.productId}</p>
