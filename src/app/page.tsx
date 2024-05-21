ab3e26ae396248817e083488f67857d3c154435a
Coffee58511867 <marobahlalele910@gmail.com>
2024-05-21 20:03:25 +0200

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col  p-24">
       <p className="text-4xl font-medium">Next JS 14 FULL COURSE</p>
+      <Link href={'/product'} className="mt-5 text-blue-600">Products</Link>
     </main>
   );
 }
