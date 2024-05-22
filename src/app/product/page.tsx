0a662074271de78fda3a01764e67b5d5d6d0fd0f
HMaroba <marobahlalele910@gmail.com>
2024-05-22 09:25:28 +0200

    Added Suspense

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,7 +1,6 @@
-"use client";
+// "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import React from "react";
 import laptopImage from "./laptop.jpeg";
 import { productsList } from "../lib/productList";
@@ -14,17 +13,30 @@ type Props = {
   currency: string;
 };
 
-export default function Products() {
-  const router = useRouter();
-  const handleNavigate = (item: Props) => {
-    router.push(`/product/${item.id}`);
-  };
+async function getData() {
+  const res = await fetch(`https://next-js14-course.vercel.app/api/products/`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Products() {
+  // const router = useRouter();
+  // const handleNavigate = (item: Props) => {
+  //   router.push(`/product/${item.id}`);
+  // };
+  const data = await getData();
+  // console.log(data);
+
   return (
     <div className="p-5">
       <p className="text-2xl font-semibold pb-10">Product Listing Below</p>
       <div className="grid lg:grid-cols-3 grid-cols-1 gap-10 w-full">
-        {productsList.map((item: Props, index: number) => (
-          <div key={index} className="" onClick={() => handleNavigate(item)}>
+        {data.map((item: Props, index: number) => (
+          <div key={index} className="">
             <div className="bg-white rounded-md shadow-md">
               <Image src={laptopImage} alt="Hello" width={500} height={250} />
               <div className="p-5 flex">
