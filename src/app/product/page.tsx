4d42d7b305a44d4ad5569e904f30ab15fa79b78d
HMaroba <marobahlalele910@gmail.com>
2024-05-22 09:29:40 +0200

    Added Suspense

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import React from "react";
 import laptopImage from "./laptop.jpeg";
 import { Suspense } from "react";
+import Loading from "../(components)/loading";
 
 type Props = {
   id: number;
@@ -34,23 +35,23 @@ export default async function Products() {
   return (
     <div className="p-5">
       <p className="text-2xl font-semibold pb-10">Product Listing Below</p>
-      <Suspense fallback={<p>Loading feed...</p>}>
-      <div className="grid lg:grid-cols-3 grid-cols-1 gap-10 w-full">
-        {data.map((item: Props, index: number) => (
-          <div key={index} className="">
-            <div className="bg-white rounded-md shadow-md">
-              <Image src={laptopImage} alt="Hello" width={500} height={250} />
-              <div className="p-5 flex">
-                <p className="text-lg">{item.title}</p>
-                <p className="text-xl font-semibold ml-auto text-blue-800">
-                  {item.currency}
-                  {item.price}
-                </p>
+      <Suspense fallback={<Loading />}>
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-10 w-full">
+          {data.map((item: Props, index: number) => (
+            <div key={index} className="">
+              <div className="bg-white rounded-md shadow-md">
+                <Image src={laptopImage} alt="Hello" width={500} height={250} />
+                <div className="p-5 flex">
+                  <p className="text-lg">{item.title}</p>
+                  <p className="text-xl font-semibold ml-auto text-blue-800">
+                    {item.currency}
+                    {item.price}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </Suspense>
     </div>
   );
