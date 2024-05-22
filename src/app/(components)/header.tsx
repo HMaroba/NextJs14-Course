3a844e419a907102a561d73ada361586ddf7cd88
HMaroba <marobahlalele910@gmail.com>
2024-05-22 12:57:08 +0200

    Added Suspense

--- src/app/(components)/header.tsx
+++ src/app/(components)/header.tsx
@@ -1,19 +1,38 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+    const currentPath = usePathname();
+    
   return (
-    <nav className="flex bg-blue-600 p-3 text-white fixed z-50">
-      <Link href={'/'} className="text-4xl font-medium">Next JS</Link>
+    <nav className="flex bg-blue-600 p-3  fixed z-50 w-full">
+      <Link href={"/"} className="text-4xl text-white font-medium">
+        Next JS
+      </Link>
       <div className="ml-auto flex gap-5 p-2">
-        <Link href={"/product"} className=" text-lg hover:text-gray-600">
+        <Link
+          href={"/product"}
+          className="text-white text-lg hover:text-gray-600"
+        >
           About
         </Link>
 
-        <Link href={"/posts"} className="  text-lg hover:text-gray-600">
+        <Link
+          href={"/posts"}
+          className={`text-lg text-white hover:text-gray-400 ${
+            currentPath === '/posts' ? 
+                'text-gray-400' 
+            : null
+          }`}
+        >
           Posts
         </Link>
-        <Link href={"/product"} className="  text-lg hover:text-gray-600">
+        <Link
+          href={"/product"}
+          className="text-white  text-lg hover:text-gray-600"
+        >
           Products
         </Link>
       </div>
