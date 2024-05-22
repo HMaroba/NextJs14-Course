c4341d3221892d2124e717e630a20c22a8793e55
HMaroba <marobahlalele910@gmail.com>
2024-05-22 09:28:42 +0200

    Added Suspense

---
+++ src/app/(components)/loading.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+        <p className='text-3xl text-green-600 font-semibold'>Loading...</p>
+    </div>
+  )
+}
