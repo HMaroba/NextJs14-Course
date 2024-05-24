04c78c4a37023364306c159205a03bc8200b7afd
Coffee58511867 <marobahlalele910@gmail.com>
2024-05-24 05:48:46 +0200

    Added database

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,12 +1,40 @@
 import { PrismaClient } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
+const prisma = new PrismaClient();
 
 export default async function Todo() {
-    const todos = await prisma?.post.findMany();
-    
+  const todos = await prisma?.post.findMany();
+
+  const addTodo = async (formData: FormData) => {
+    "use server";
+
+    const description = formData.get("description");
+    await prisma.post.create({
+      data: {
+        description: description as string,
+      },
+    });
+  };
+
   return (
-    <div className="flex min-h-screen flex-col">
-      
+    <div className="flex min-h-screen flex-col justify-center items-center p-24">
+      <p className="text-3xl">Todos Page</p>
+      <form action={addTodo} className="flex flex-col w-3/5 p-10">
+        <input
+          className="p-3 w-full border border-gray-400 rounded-md"
+          placeholder="Enter todo"
+          name="description"
+          required
+        />
+        <button className="w-full p-2 bg-green-400 text-white rounded-lg mt-5">
+          Save
+        </button>
+      </form>
+
+      <ul className="list-disc">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.description}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
