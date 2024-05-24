a426b172bd7217e9f1b0a804fcacca4baf9cc5d4
Coffee58511867 <marobahlalele910@gmail.com>
2024-05-24 06:00:07 +0200

    Added database

--- src/app/actions/action.ts
+++ src/app/actions/action.ts
@@ -1,9 +1,11 @@
+"use server";
+
 import { PrismaClient } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 const prisma = new PrismaClient();
 
 export const addTodo = async (formData: FormData) => {
-    "use server";
+   
 
     const description = formData.get("description");
     await prisma.post.create({
