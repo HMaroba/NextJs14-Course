fd57b3cfd3b3421a48057aa0c64f54e9d6301f2f
Coffee58511867 <marobahlalele910@gmail.com>
2024-05-24 06:07:40 +0200

    Added database

--- src/app/(components)/button.tsx
+++ src/app/(components)/button.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useFormStatus } from "react-dom";
 
 export default function Button() {
+  const { pending } = useFormStatus();
   return (
     <button className="w-full p-2 bg-blue-500 text-white rounded-lg mt-5">
-      Save
+      {pending ? "Adding todo ...." : "Save"}
     </button>
   );
 }
--- src/app/(components)/form.tsx
+++ src/app/(components)/form.tsx
@@ -1,19 +1,19 @@
+"use client";
 
-'use client'
-
-import React from 'react'
-import { addTodo } from '../actions/action'
+import React from "react";
+import { addTodo } from "../actions/action";
+import Button from "./button";
 
 export default function Form() {
   return (
     <form action={addTodo} className="flex flex-col w-3/5 p-10">
-        <input
-          className="p-3 w-full border border-gray-400 rounded-md"
-          placeholder="Enter todo"
-          name="description"
-          required
-        />
-
-      </form>
-  )
+      <input
+        className="p-3 w-full border border-gray-400 rounded-md"
+        placeholder="Enter todo"
+        name="description"
+        required
+      />
+      <Button />
+    </form>
+  );
 }
